0e4e0dd7b67066e8c8a0afb08ccee40344ceef91
Jonas Betzendahl <jbetzend@techfak.uni-bielefeld.de>
2015-06-13 18:41:03 +0200

    Implemented a sieve. Now much faster.

--- Main.hs
+++ Main.hs
@@ -1,9 +1,18 @@
 module Main where
 
+import System.IO
 import System.Environment          (getArgs)
+
 import Data.List                   (findIndex)
 import Data.Digits                 (digits, unDigits)
+
+import Control.Monad               (unless)
+import Control.Concurrent          (forkIO)
+import Control.Concurrent.Async    (race_)
 import Control.Parallel.Strategies (parListChunk, using, rseq)
+
+nonSillyNumbers :: Integer -> [Integer]
+nonSillyNumbers n = n:(n+1):(n+25):(n+36):(nonSillyNumbers (n+60))
 
 convertBase :: Integral a => a -> a -> [a] -> [a]
 convertBase from to = digits to . unDigits from
@@ -11,18 +20,26 @@ convertBase from to = digits to . unDigits from
 valid :: Integral a => a -> a -> [a] -> Bool
 valid from to = (all (\d -> (d < 2))) . (convertBase from to)
 
+-- Base 2 always valid.
 check :: Integer -> Bool
-check n = and [valid 6 b m | b <- [3,4,5]]
-  where
-    m = (convertBase 10 2) (digits 10 n)
+check n = let m = digits 10 n
+           in (valid 10 6 m) && (valid 10 5 m) && (valid 10 4 m) && (valid 10 3 m)
 
+-- Assumes argument is a multiple of 60 (for non-silly-number-generation)
 main :: IO ()
-main = do [arg] <- getArgs
-          loop (read arg)
+main = do hSetBuffering stdin NoBuffering
+          [arg] <- getArgs
+          race_ loop (calc (read arg))
   where
-    loop :: Integer ->  IO ()
-    loop n = do let s = n * 1000000
-                let t = s + 1000000
-                let bs = [check m | m <- [s..t]] `using` parListChunk 1000 rseq
-                if or bs then print $ "Found! Base is " ++ show s ++ ", offset is " ++ show (findIndex (== True) bs)
-                         else do { putStrLn $ "No match for n = " ++ show n ; loop (n+1) }
+    loop :: IO ()
+    loop = do c <- getChar
+              if (c == 'q') then putStrLn " Exiting."
+                            else putStrLn "Exit at any time by pressing \"Q\"" >> loop
+
+calc :: Integer -> IO ()
+calc n = do let s = take 6666668 (nonSillyNumbers n)
+            let bs = [check m | m <- s] `using` parListChunk 100000 rseq
+            if or bs then print $ "Found!"
+                     else do let ls = last s
+                             putStrLn $ "No match for n <= " ++ show ls
+                             calc ls
